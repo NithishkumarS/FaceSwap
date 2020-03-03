26b8ed68f75463c213a6065257c50f02506647f5
Zachary Zimits <zachary@zimits.com>
2020-03-03 18:06:38 -0500

    Changed setup of triangulation to make the the points easier to access in future steps

--- Wrapper.py
+++ Wrapper.py
@@ -46,32 +46,66 @@ def drawTriangles(image,triangles,rect):
 			cv2.line(image,pt1,pt2,(255,255,255),1)
 			cv2.line(image,pt2,pt3,(255,255,255),1)
 			cv2.line(image,pt3,pt1,(255,255,255),1)
-			modified_triangles.append(t)
+			modified_triangles.append([pt1,pt2,pt3])
 	return image,modified_triangles
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('Descriptors/shape_predictor_68_face_landmarks.dat')
+def calcU(point1,point2):
+	x1 = point1[0]
+	y1 = point1[1]
+	x2 = point2[0]
+	y2 = point2[1]
+	r_2 = (np.linalg.norm([x1,y1]-[x2-y2]))**2
+	U = r_2 * np.log(r_2)
+	return U
+
+def calcK(points1,points2):
+	K = np.zeros((len(points1,len(points2))),dtype=np.float32)
+	for i,pointi in enumerate(points1):
+		for j,poinj in enumerate(points2):
+			K[i,j] = calcU(pointi,pointj)
+	return K
+
+def getP(pointsi):
+	ones = np.zeros((len(pointsi),1),np.float32) + 1;
+	P = np.hstack((pointsi,ones))
+	Pt = np.transpose(P)
+	return P,Pt
+
+
+def findFeatures(image):
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor('Descriptors/shape_predictor_68_face_landmarks.dat')
+
+	size = image.shape
+	bound_rect = (0,0,size[1],size[0])
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# cv2.imshow('face',image)
+	# cv2.waitKey(0)
+
+	rects = detector(gray,1)
+	for (i, rect) in enumerate(rects):
+		pt1,pt2 = rect2box(rect)
+		shape = predictor(gray,rect)
+		shape = shape_to_np(shape)
+		subdiv = cv2.Subdiv2D(bound_rect)
+		for (x,y) in shape:
+			cv2.circle(image,(x,y),1, (0,0,255),-1)
+			subdiv.insert((x,y))
+		cv2.rectangle(image,pt1,pt2,(255,0,0))
+		triangles = subdiv.getTriangleList()
+		image,triangles = drawTriangles(image,triangles,bound_rect)
+	print(np.shape(triangles))
+	cv2.imshow('face',image)
+	cv2.waitKey(0)
+	return shape,triangles
+
+
+
 
 image = cv2.imread('Data/2faces.jpeg')
-size = image.shape
-bound_rect = (0,0,size[1],size[0])
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('face',image)
-# cv2.waitKey(0)
+shape,triangles = findFeatures(image)
 
-rects = detector(gray,1)
-for (i, rect) in enumerate(rects):
-	pt1,pt2 = rect2box(rect)
-	shape = predictor(gray,rect)
-	shape = shape_to_np(shape)
-	subdiv = cv2.Subdiv2D(bound_rect)
-	for (x,y) in shape:
-		cv2.circle(image,(x,y),1, (0,0,255),-1)
-		subdiv.insert((x,y))
-	cv2.rectangle(image,pt1,pt2,(255,0,0))
-	triangles = subdiv.getTriangleList()
-	image,triangles = drawTriangles(image,triangles,bound_rect)
-print(np.shape(triangles))
-cv2.imshow('face',image)
-cv2.waitKey(0)
+
+# Face Warping using Thin Plate Spline
+
 
